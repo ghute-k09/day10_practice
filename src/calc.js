2e600007b8a015c834cf60d94f649d990725766b
Kailas Ghute <kpaghute@gmail.com>
2023-06-10 22:28:09 +0530

    commit

--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,8 @@
 function sum(n1 , n2){
     return n1 + n2;
 }
+function substraction(n1 , n2){
+    return n1 - n2;
+}
 
-export {sum};
+export {sum, substraction};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import {sum} from "./calc.js";
+import { readFileSync } from 'fs';
 
-let output = sum(10 ,20);
-
-
-console.log(output);
+let filePath = 'D:/PG-DAC-MAR23/WPT/WPT SANTOSH MONDAL/day10 practice/package.json';
+let fileData = readFileSync(filePath, {encoding : "utf-8"});
+console.log(fileData);
