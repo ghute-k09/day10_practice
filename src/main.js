787abac3cc0d41be4c90b4b7166b75c0fb91f2fd
Kailas Ghute <kpaghute@gmail.com>
2023-06-10 23:40:48 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { writeFileSync} from 'fs';
 
 let filePath = "D:/PG-DAC-MAR23/WPT/WPT SANTOSH MONDAL/day10 practice/output.txt";
-let data = "good night";
+let data = `Lorem Ipsum Or Dummy Content Generator ... Then,
+            you can choose the number of words and paragraphs for your
+            dummy content and execute the plan accordingly.`;
+            
 writeFileSync(filePath, data);
 console.log("write success!");
